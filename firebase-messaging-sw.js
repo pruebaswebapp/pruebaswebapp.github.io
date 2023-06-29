9ac3f21823e53e75ded8f7e3e59d41de0b529c3c
MariavGuadalupe <132510330+MariavGuadalupe@users.noreply.github.com>
2023-06-29 12:14:12 -0600

    lugar  firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,37 +1,3 @@
-console.log("Ingreso wk version 2");
-importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
-let pathToJson = new URL(location).searchParams.get("config");
-console.log(pathToJson); 
-console.log(decodeURIComponent(pathToJson)); 
-let firebaseConfig = JSON.parse(decodeURIComponent(pathToJson));
-firebase.initializeApp(firebaseConfig); 
-const messaging =  firebase.messaging();
-console.log("ingreso registrando mensaje 2");
-
-messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js...] Received background message ', payload);
-  const notification = payload.data;
-
-  notification.titulo= "Alegra";
-  notification.mensaje= "Construye el Reino de Dios";
-  notification.imagen= "https://mariavguadalupe.github.io/Alegra512.png";
-
-  console.log('titulo 2', notification.titulo);
-  console.log('mensaje 2', notification.mensaje);
-  console.log('imagen 2', notification.imagen);
-
-  
-
-  // Customize notification here
-	self.registration.showNotification(notification.titulo, {
-        body: notification.mensaje,
-        icon: notification.imagen
-    });      
-});
-
-
-
 
 // Instalar SW PWA
 
@@ -93,3 +59,42 @@ self.addEventListener("fetch", function(ev) {
 });
 
 */
+
+
+
+// //////////////////////////////
+
+console.log("Ingreso wk version 2");
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+let pathToJson = new URL(location).searchParams.get("config");
+console.log(pathToJson); 
+console.log(decodeURIComponent(pathToJson)); 
+let firebaseConfig = JSON.parse(decodeURIComponent(pathToJson));
+firebase.initializeApp(firebaseConfig); 
+const messaging =  firebase.messaging();
+console.log("ingreso registrando mensaje 2");
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js...] Received background message ', payload);
+  const notification = payload.data;
+
+  notification.titulo= "Alegra";
+  notification.mensaje= "Construye el Reino de Dios";
+  notification.imagen= "https://mariavguadalupe.github.io/Alegra512.png";
+
+  console.log('titulo 2', notification.titulo);
+  console.log('mensaje 2', notification.mensaje);
+  console.log('imagen 2', notification.imagen);
+
+  
+
+  // Customize notification here
+	self.registration.showNotification(notification.titulo, {
+        body: notification.mensaje,
+        icon: notification.imagen
+    });      
+});
+
+
+
