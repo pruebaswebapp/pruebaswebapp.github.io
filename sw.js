6e7428a340fc120a8e09630c6bef49784621d57d
pruebaswebapp <138265194+pruebaswebapp@users.noreply.github.com>
2023-08-18 18:53:26 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,10 +3,7 @@ var swPath;
 var urlObject = new URL(location);
 var host;
 
-console.log("1 "+swPath);
-console.log("2" + urlObject);
 
-console.log("3 " + host);
 
 if (urlObject.searchParams.get("swPath")) {
     swPath = urlObject.searchParams.get("swPath");
@@ -28,11 +25,7 @@ else {
     swPath = host + version + "/sw.js";
 }
 
-console.log("4" +swPath);
-console.log("5 " + urlObject);
 
-console.log("6 " + host);
 
 importScripts(swPath);
-console.log("7 " + swPath);
 
